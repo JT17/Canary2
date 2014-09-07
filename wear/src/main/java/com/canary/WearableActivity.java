ecf64fc8fd584f97b39472f509294b6f9a9e644e
Jonathan <tiaojon@umich.edu>
2014-09-06 20:05:04 -0400

    SHould have connection from wearable to phone

--- wear/src/main/java/com/canary/WearableActivity.java
+++ wear/src/main/java/com/canary/WearableActivity.java
@@ -1,8 +1,6 @@
 package com.canary;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +8,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.view.WatchViewStub;
@@ -18,9 +17,16 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
 import java.util.concurrent.CountDownLatch;
 
-public class WearableActivity extends Activity implements SensorEventListener {
+public class WearableActivity extends Activity implements SensorEventListener, MessageApi.MessageListener,GoogleApiClient.ConnectionCallbacks {
     private static final String TAG = WearableActivity.class.getName();
     private SensorManager mSensorManager;
     private Sensor mHeartRateSensor;
@@ -31,6 +37,9 @@ public class WearableActivity extends Activity implements SensorEventListener {
     PendingIntent pendingIntent;
     private CountDownLatch latch;
     double firstMeasure;
+    boolean sendMessage;
+    private GoogleApiClient mGoogleApiClient;
+    private static final String GET_HELP = "/text_for_help";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,17 +55,22 @@ public class WearableActivity extends Activity implements SensorEventListener {
         });
 
         mContext = this;
-        Log.v("LKSFJ", "a;dskjfa;");
         pendingIntent =
                 PendingIntent.getActivity(this, 0, new Intent(this, WearableActivity.class), 0);
         mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
         mHeartRateSensor = mSensorManager.getDefaultSensor(SENSOR_TYPE_HEARTRATE);
+        sendMessage = false;
 
     }
     @Override
     protected void onStart() {
         super.onStart();
         mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .build();
+        mGoogleApiClient.connect();
     }
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -68,8 +82,10 @@ public class WearableActivity extends Activity implements SensorEventListener {
             Log.d(TAG, "sensor event: " + sensorEvent.accuracy + " = " + sensorEvent.values[0]);
             TextView view = (TextView) findViewById(R.id.text);
             view.setText(Float.toString(sensorEvent.values[0]));
-            if (sensorEvent.values[0] > 1.3 * firstMeasure)
+            if (sensorEvent.values[0] > 1.3 * firstMeasure) {
                 Toast.makeText(this, "alerting authorities", Toast.LENGTH_SHORT);
+                sendMessage = true;
+            }
         } catch (InterruptedException e) {
             Log.e(TAG, e.getMessage(), e);
         }
@@ -82,24 +98,63 @@ public class WearableActivity extends Activity implements SensorEventListener {
 
     @Override
     protected void onStop() {
+        if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
         super.onStop();
-
         mSensorManager.unregisterListener(this);
     }
     public void onClick(View v){
         Log.v("Viewpoop", Integer.toString(v.getId()));
         switch(v.getId()){
-            case (R.id.button):
+            case (R.id.wearablemain):
                 Log.v("Button", "Touched");
-                Notification notif = new Notification.Builder(mContext)
-                        .setContentTitle("My Notification")
-                        .setContentText("This is a notification")
-                        .setSmallIcon(R.drawable.ic_plusone_small_off_client)
-                        .build();
-
-                NotificationManager notificationManger =
-                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManger.notify(0, notif);
+                sendMessage = true;
         }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+        if(sendMessage == true){
+            new RequestDataAsyncTask().execute();
+        }
+    }
+    public class RequestDataAsyncTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... args) {
+            NodeApi.GetConnectedNodesResult nodes =
+                    Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+
+            if (nodes.getNodes().size() > 0) {
+                Wearable.MessageApi.sendMessage(
+                        mGoogleApiClient, nodes.getNodes().get(0).getId(), GET_HELP, "".getBytes()).setResultCallback(
+                        new ResultCallback<MessageApi.SendMessageResult>() {
+                            @Override
+                            public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                                if (!sendMessageResult.getStatus().isSuccess()) {
+                                    Log.d("WATCH OUTPUT", "Failed to send message with status code: "
+                                            + sendMessageResult.getStatus().getStatusCode());
+                                } else {
+                                    Log.d("WATCH OUTPUT", "Successfully requested train times");
+                                }
+                            }
+                        }
+                );
+            }
+
+            return null; //return value doesn't matter...
+        }
+    }
+    //Ignore this for now
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+    //Doesn't need to be implemented b/c phone shouldn't talk to watch as of now
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
     }
 }
